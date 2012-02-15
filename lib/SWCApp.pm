c5745ffeafa694d9d01f87e89b81b4ebf2ee4325
dryman <idryman@gmail.com>
2012-02-15 16:31:07 +0800

    server can generate ajax content

--- lib/SWCApp.pm
+++ lib/SWCApp.pm
@@ -1,14 +1,14 @@
 package SWCApp;
 use 5.010;
 use utf8;
-#use encoding 'utf8';
-use Encode qw(is_utf8);
 use Dancer ':syntax';
-use File::Find;
-use FindBin qw($Bin);
+use FindBin '$Bin';
+use List::Util 'shuffle';
+use SWCApp::DB;
 set serializer => 'JSON';
 
 our $VERSION = '0.1';
+
 
 get '/' => sub {
     template 'index';
@@ -16,8 +16,56 @@ get '/' => sub {
 
 get '/example' => sub {
   { 
-    hello => "The title is changed",
+    hello => "The title is changed\n",
     cool => "haha",
+  }
+};
+
+get '/test' => sub {
+  my $schema = SWCApp::DB->connect('dbi:SQLite:dbname=development.db',"","",{sqlite_unicode=>1});
+  my $r_rs = $schema->resultset('RandomSet');
+
+  # choose set_id
+  my $random_groups = $r_rs
+    ->search({},{group_by=>'set_id'})
+    ->count;
+  my $set_id = int(rand($random_groups))+1;
+
+  # get set of articles (with title, cluster, and content)
+  my @randoms = $r_rs->search({set_id=>$set_id},{order_by => "ord"})->all;
+  my @articles = map {{
+    id => $_->article->id,
+    title => $_->article->title,
+    cluster => $_->article->cluster,
+    content => $_->article->content,
+  }} @randoms;
+
+  # generate test snippets
+  my @r_snippets = 
+    shuffle 
+    map {$_->article->snippet}
+    $r_rs->search({set_id => $set_id})->all;
+  my @n_snippets =
+    shuffle
+    map {$_->snippet}
+    $schema->resultset('Article')->search({
+      cluster => 0,
+      id => { -not_in =>
+        $r_rs
+          ->search({set_id=>$set_id})
+          ->get_column('article_id')
+          ->as_query
+      },
+    })->all;
+  my @r_nums = shuffle 0..4;
+  my @selected_snippets = (@r_snippets[0..2], @n_snippets[0..1]);
+  my @test_snippets = @selected_snippets[@r_nums];
+  my @answers = map {$_ <= 2 ? 1: 0} @r_nums;
+
+  {
+    articles => \@articles,
+    test_snippets => \@test_snippets,
+    answers => \@answers,
   }
 };
 
--- lib/SWCApp/DB/Result/TimeStamp.pm
+++ lib/SWCApp/DB/Result/TimeStamp.pm
@@ -5,7 +5,7 @@ __PACKAGE__->table('time_stamp');
 __PACKAGE__->load_components(qw|InflateColumn::DateTime|);
 __PACKAGE__->add_columns(
   id =>       +{qw|data_type integer  is_nullable 0  is_auto_increment  1|},
-  time =>     +{qw|data_type datetime is_nullable 0 floating_tz_ok => 1|},
+  time =>     +{qw|data_type datetime is_nullable 0 floating_tz_ok 1|},
   post_option => +{qw|data_type integer  is_nullable 0 default_value 0|},
   article_id =>  +{qw|data_type integer  is_nullable 0  is_foreign_key 1|},
   user_id =>     +{qw|data_type integer  is_nullable 0  is_foreign_key 1|},
